7f62bc832e31c8925ccc14cbd9bfadd11996fd27
chungtd89 <49780689+chungtd89@users.noreply.github.com>
2020-12-16 14:24:26 +0700

    Fix webdriver closing

--- main.py
+++ main.py
@@ -12,30 +12,28 @@ from selenium.webdriver.common.keys import Keys
 import time
 import csv
 
-driver = webdriver.Firefox(executable_path=r'/home/jt/Desktop/chotot/geckodriver')
+driver = webdriver.Firefox(executable_path=r'D:\GitHub\ncov_moh_retrieve\geckodriver.exe')
 driver.get("https://ncov.moh.gov.vn/en/web/guest/dong-thoi-gian")
 
-stop = 0
-while (stop != 1):
+while (1):
 
     time.sleep(3)
 
     elems_head = driver.find_elements_by_class_name('timeline-head')
     elems_content = driver.find_elements_by_class_name('timeline-content')
 
-    elems_len = len (elems_head)
+    elems_len = len(elems_head)
 
     for i in range(elems_len):
-        print (elems_head[i].text)
-        print (elems_content[i].text)
-        
-        with open('vnncovfile.csv', mode='a+') as f:
+        #print (elems_head[i].text)
+        #print (elems_content[i].text)
+
+        with open('vnCovid19Summary.csv', mode='a+', encoding="utf-8") as f:
             fwrite = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
             fwrite.writerow([elems_head[i].text, elems_content[i].text])
 
-    if (driver.find_element_by_link_text('Next').get_attribute("tabindex") == -1):
-        stop = 1
-        driver.close()
+    if int(driver.find_element_by_link_text('Next').get_attribute("tabindex")) == -1:
+        driver.quit()
         break
-        
+
     driver.find_element_by_link_text('Next').click()
